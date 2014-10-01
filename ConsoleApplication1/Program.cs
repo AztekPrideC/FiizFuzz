3b73576eb5525913c309c2f5d2408d08e31a60fe
AztekPrideC <aztekpridec@gmail.com>
2014-10-01 10:42:39 -0500

    programa final

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {    if ((i % 3==0) && (i % 5==0)) {
+                Console.WriteLine("FizzFuzz");
+
+            } else if(i%3==0){
+                   Console.WriteLine("Fizz");
+               }
+               else if (i%5==0) {
+                  Console.WriteLine("Fuzz");
+               }  else Console.WriteLine(i);
+            } Console.ReadLine();
+            }
+
+
+        }
+    }
+
